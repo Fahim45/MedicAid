4d5696ba9be0ff9b27994a1ad2724ae95153effc
FAHIM FAISAL <slyfox741@gmail.com>
2021-02-22 22:17:58 +0600

    going signup to login

--- app/src/main/java/com/cse25/medicaid/features/SignUpActivity.java
+++ app/src/main/java/com/cse25/medicaid/features/SignUpActivity.java
@@ -1,4 +1,40 @@
 package com.cse25.medicaid.features;
 
-public class SignUpActivity {
+import com.cse25.medicaid.R;
+import com.cse25.medicaid.support.BaseActivity;
+import com.cse25.medicaid.support.Navigator;
+
+public class SignUpActivity extends BaseActivity {
+    @Override
+    protected String setActionBarTitle() {
+        return "SignUp";
+    }
+
+    @Override
+    protected String setDebugTag() {
+        return "SignUpActivity";
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_signup;
+    }
+
+    @Override
+    protected boolean doYouWantToEnableBackButton() {
+        return false;
+    }
+
+    @Override
+    protected void initViewComponents() {
+
+    }
+
+    @Override
+    protected void addObserversAndHandlers() {
+        findViewById(R.id.login_button).setOnClickListener(v -> {
+            Navigator.startActivity(SignUpActivity.this, LogInActivity.class);
+            finish();
+        });
+    }
 }
