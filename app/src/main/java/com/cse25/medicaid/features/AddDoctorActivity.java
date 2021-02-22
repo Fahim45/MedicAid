ebd1e9d9dbabafc1470532f7bd3fca1dedcb398a
FAHIM FAISAL <slyfox741@gmail.com>
2021-02-22 22:35:22 +0600

    Adding Add doctor activity in manifest

--- app/src/main/java/com/cse25/medicaid/features/AddDoctorActivity.java
+++ app/src/main/java/com/cse25/medicaid/features/AddDoctorActivity.java
@@ -1,4 +1,36 @@
 package com.cse25.medicaid.features;
 
-public class AddDoctorActivity {
+import com.cse25.medicaid.R;
+import com.cse25.medicaid.support.BaseActivity;
+
+public class AddDoctorActivity extends BaseActivity {
+    @Override
+    protected String setActionBarTitle() {
+        return "Add Doctor";
+    }
+
+    @Override
+    protected String setDebugTag() {
+        return "AddDoctorActivity";
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_add_doctor;
+    }
+
+    @Override
+    protected boolean doYouWantToEnableBackButton() {
+        return true;
+    }
+
+    @Override
+    protected void initViewComponents() {
+
+    }
+
+    @Override
+    protected void addObserversAndHandlers() {
+
+    }
 }
